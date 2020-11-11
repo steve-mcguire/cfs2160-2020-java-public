89961de87780f9558db709de8458f8c690545470
Steve McGuire <s.mcguire@hud.ac.uk>
2020-11-11 17:09:05 +0000

    Week8

---
+++ Week8/HelloWorld.java
@@ -0,0 +1,14 @@
+package Week8;
+
+public class HelloWorld {
+
+    private void printHello(){
+        System.out.println("hello. world");
+    }
+
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.printHello();
+    }
+
+}
---
+++ Week8/Student.java
@@ -0,0 +1,70 @@
+package Week8;
+
+public class Student {
+    private String studentID;
+    private String name;
+    private String DOB;
+    private String emailAddress;
+    private String patName;
+    private String patEmailAddress;
+
+    public Student(String studentID, String name) {
+        this.studentID = studentID;
+        this.name = name;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPatName() {
+        return patName;
+    }
+
+    public void setPatName(String patName) {
+        this.patName = patName;
+    }
+
+    public String getPatEmailAddress() {
+        return patEmailAddress;
+    }
+
+    public void setPatEmailAddress(String patEmailAddress) {
+        this.patEmailAddress = patEmailAddress;
+    }
+
+    public static void main(String[] args) {
+        Student steve = new Student("u0670824", "Steve McGuire");
+        System.out.println(steve.getName());
+        System.out.println(steve.getStudentID());
+
+    }
+}
