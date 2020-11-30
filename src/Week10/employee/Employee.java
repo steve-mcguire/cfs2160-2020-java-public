2fb524c7ed5a23e90f9cbc7178e9e228803eb19d
Steve McGuire <s.mcguire@hud.ac.uk>
2020-11-30 13:04:54 +0000

    Added Week 10 Employee

---
+++ src/Week10/employee/Employee.java
@@ -0,0 +1,41 @@
+package Week10.employee;
+
+public class Employee {
+    private String name;
+    private String emailAddress;
+    private int idNumber;
+
+    public Employee(String name, String emailAddress, int idNumber) {
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.idNumber = idNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
