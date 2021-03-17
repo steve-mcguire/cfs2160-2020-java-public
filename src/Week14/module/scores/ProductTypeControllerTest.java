4c0ca0be07813e4ede8aeada9658d5997c520e73
Steve McGuire <s.mcguire@hud.ac.uk>
2021-03-17 15:10:54 +0000

    week 22 testing and practical code

--- src/Week14/module/scores/ProductTypeControllerTest.java
+++ src/Week14/module/scores/ProductTypeControllerTest.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ModuleControllerTest {
+class ProductTypeControllerTest {
     private ModuleController mc;
 
     @BeforeEach
---
+++ src/Week20/practicalcode/CalculatorTest.java
@@ -0,0 +1,29 @@
+package Week20.practicalcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addition() {
+    }
+
+    @Test
+    void multiplication() {
+    }
+
+    @Test
+    void subtraction() {
+    }
+
+    @Test
+    void division() {
+    }
+}
